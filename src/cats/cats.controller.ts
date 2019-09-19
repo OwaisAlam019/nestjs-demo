ae4747d421566c024b6d6483a1e6b081a2b3d91b
Owais Alam <ovais.alam@sixlogs.com>
2019-09-19 11:40:46 +0500

    get and post routes setup with response

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, Req, Post, Body, Header } from '@nestjs/common';
+import { Request } from 'express';
+import { CreateCatDto } from './create-cat.dto'
+
+
+
+@Controller('cats')
+export class CatsController {
+    // @Get()
+    // findAll(@Req() req: Request): string {
+    //     console.log(req,"reqiest")
+    //     return "this action from cats controller";
+    // }
+
+    @Post()
+    // @Header('Content-Type', 'application/json')
+   async create(@Body() catObj : CreateCatDto)  {
+       console.log(catObj,"yo")
+        return {
+            name:catObj.name,
+            age:catObj.age,
+            breed:catObj.breed
+        }
+    } 
+}
+
