23f8e49074faf823c4c101a7ce839923ed9f1e2e
OwaisAlam019 <owaisstar@gmail.com>
2019-09-19 22:48:59 +0500

    database connection with mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
+// import { CatsController } from './cats/cats.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+import { join } from 'path';
+// import { CatsService } from './cats/cats.services';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
+  imports: [
+    TypeOrmModule.forRoot({
+      "type":"mysql",
+      "host":"localhost",
+      "port":3306,
+      "username":"root",
+      "password":"123",
+      "database":"cats",
+      "entities":[join(__dirname + '/**/**.entity{.ts,.js}')],
+      // "entities":[join(__dirname + '/cats/cats.entity.ts')],
+      "synchronize":true
+    }),
+    CatsModule],
+  controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly connection:Connection){
+    if(connection)
+    console.log("db connected")
+    console.log(join(__dirname + '**/**.entity{.ts,.js}'))
+  }
+}
